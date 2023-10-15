7c66ae34c3b3999b529fd735a051ded769fa4950
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2023-10-15 16:06:11 -0300

    feat: crud endereço

---
+++ FilmesAPI/Profiles/EnderecoProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Profiles;
+
+public class EnderecoProfile : Profile
+{
+    public EnderecoProfile() 
+    {
+        CreateMap<CreateEnderecoDto, Endereco>();
+        CreateMap<UpdateEnderecoDto, Endereco>();
+        CreateMap<Endereco, ReadEnderecoDto>();
+    }
+}
